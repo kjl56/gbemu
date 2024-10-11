8a552ef726ba1e5c047bf03823904db379d45b93
Kyle Lugo <kjl56@zips.uakron.edu>
2024-10-11 01:52:21 -0400

    add sdl2 dependency

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,13 @@ mod memory;
 mod gpu;
 use crate::cpu::cpu::CPU;
 
+extern crate sdl2;
+
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use std::time::Duration;
+
 fn main() {
   let args: Vec<String> = env::args().collect();
   let rom_file = &args[1];
@@ -23,10 +30,39 @@ fn main() {
     cpu.bus.memory[x] = cartrige[x];
     x += 1;
   }
-  //println!("{:?}", cpu.bus.memory);
+
+  let sdl_context = sdl2::init().unwrap();
+  let video_subsystem = sdl_context.video().unwrap();
+
+  let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+      .position_centered()
+      .build()
+      .unwrap();
+
+  let mut canvas = window.into_canvas().build().unwrap();
+
+  canvas.set_draw_color(Color::RGB(0, 255, 255));
+  canvas.clear();
+  canvas.present();
+  let mut event_pump = sdl_context.event_pump().unwrap();
   let mut i = 0;
-  while i < 500000 {
-    cpu.step();
-    i = i + 1;
-  }
+    'running: loop {
+      i = (i + 1) % 255;
+      canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+      canvas.clear();
+      for event in event_pump.poll_iter() {
+          match event {
+              Event::Quit {..} |
+              Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                  break 'running
+              },
+              _ => {}
+          }
+      }
+      // The rest of the game loop goes here...
+      cpu.step();
+
+      canvas.present();
+      ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
 }
